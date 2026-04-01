7a093259023f0c996630d2b92776f3605156f846
VikingTradingBots <investimentos.viking@gmail.com>
2026-04-01 02:54:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/vikingtradingbots1/',
+  base: '/vikingtradingbots/',
   server: {
     host: "::",
     port: 8080,
